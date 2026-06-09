d75c265c872ef847e432fddf458a0a09fd78941f
GhazalehManj <126309136+GhazalehManj@users.noreply.github.com>
2026-06-09 17:06:27 -0400

    Update ui/views/congratulations_page.py
    Co-authored-by: ThomasHMAC <39802916+ThomasHMAC@users.noreply.github.com>

--- ui/views/congratulations_page.py
+++ ui/views/congratulations_page.py
@@ -114,7 +114,7 @@ def show_congratulations_page(
 				st.switch_page(entrypoint_rel_path)
 			st.rerun()
 	with col3:
-if st.button(MESSAGES["start_over_button"], width="stretch"):
+		if st.button(MESSAGES["start_over_button"], width="stretch"):
 			SessionManager.set_landing_page_complete(False)
 			SessionManager.set_qc_cohort_order([])
 			SessionManager.set_participant_ids([])
